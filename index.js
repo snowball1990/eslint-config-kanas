7a79db6e282fc46721504d788298b87cb933f991
snowball1990 <snowballer0705@gmail.com>
2019-04-21 18:25:58 +0800

    :wrench: change config

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ module.exports = {
     sourceType: "module",
     parser: "babel-eslint"
   },
-  plugins: ["vue"],
-  extends: ["plugin:vue/base"],
+  plugins: ["vue", "prettier"],
+  // https://github.com/vuejs/vue-cli/pull/2932/commits/fbb55f40e4c0ea1ca91707a1a81db5f0a516cc4b
+  extends: [
+    "plugin:vue/base",
+    // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+    "plugin:prettier/recommended",
+    // https://github.com/prettier/eslint-config-prettier/blob/master/README.md#installation
+    "prettier/vue"
+  ],
   env: {
     browser: true
   },
